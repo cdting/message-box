0c950b5ba5215f0e5704b685f23d4d7dbf171fb2
Tin <memailsg@gmail.com>
2017-08-29 09:28:05 +0800

    添加右下角提示框

--- js/message_box.js
+++ js/message_box.js
@@ -2,34 +2,40 @@
     'use strict';
 
     function Message() {
-
+        // this.i = 0;
     };
 
     Message.prototype = {
+        parentDocument: function() {
+            return window.parent.document || window.document;
+        },
         idNode: function(id) {
-            return document.getElementById(id);
+            return this.parentDocument().getElementById(id);
         },
         eleNode: function(ele) {
-            return document.getElementsByTagName(ele)[0];
+            return this.parentDocument().getElementsByTagName(ele)[0];
         },
         getEvent: function(event) {
             return event || window.event;
         },
-        createEle: function(ele, className, id, parentEle, text) {
-            var newEle = document.createElement(ele);
-            newEle.className = className;
-            newEle.id = id;
-
-            if (text) {
-                var t = document.createTextNode(text);
-                newEle.appendChild(t);
+        createEle: function() {
+            var params = arguments;
+            var newEle = document.createElement(params[0]);
+            if (params.length >= 4) {
+                newEle.className = params[1];
+                newEle.id = params[2];
+                if (params.length === 5 && params[3].indexOf('_') != -1) {
+                    var t = document.createTextNode(params[4]);
+                    newEle.appendChild(t);
+                };
+                if (params[3].indexOf('_') === -1) {
+                    newEle.style.backgroundColor = params[4];
+                    this.eleNode(params[3]).appendChild(newEle);
+                } else {
+                    this.idNode(params[3]).appendChild(newEle);
+                };
             };
 
-            if (parentEle.indexOf('_') === -1) {
-                this.eleNode(parentEle).appendChild(newEle);
-            } else {
-                this.idNode(parentEle).appendChild(newEle);
-            };
         },
         bindEventOfID: function(bindEle, bindType, fn) {
             var idEle = this.idNode(bindEle);
@@ -47,18 +53,26 @@
                 event.cancelBubble = true;
             }
         },
-        removeEle: function(clickEleID, removeEle) {
+        removeEle: function(clickEleID, removeEle, animationStyle, timer) {
             var that = this;
             this.bindEventOfID(clickEleID, "click", function(event) {
                 var clickParentClassName = that.idNode(clickEleID).parentNode;
                 var parentClassNameArray = clickParentClassName.className.split(' ');
-                parentClassNameArray.length > 1 ? clickParentClassName.className = parentClassNameArray[0] + " remove-alert-box" : parentClassNameArray.className = clickParentClassName;
+                parentClassNameArray.length > 1 ? clickParentClassName.className = parentClassNameArray[0] + " " + animationStyle : parentClassNameArray.className = clickParentClassName;
                 setTimeout(function() {
                     var node = that.idNode(removeEle);
                     node.parentNode.removeChild(node);
                     that.stopBubble(event);
-                }, 300);
+                }, timer);
             });
+        },
+        timeoutRemoveEle: function(removeEle, animationStyle, timer) {
+            var clickParentClassName = this.idNode(removeEle);
+            var parentClassNameArray = clickParentClassName.className.split(' ');
+            parentClassNameArray.length > 1 ? clickParentClassName.className = parentClassNameArray[0] + " " + animationStyle : parentClassNameArray.className = clickParentClassName;
+            setTimeout(function() {
+                clickParentClassName.parentNode.removeChild(clickParentClassName);
+            }, timer);
         }
     };
 
@@ -74,9 +88,33 @@
 
         this.createEle("button", "box-button", "box_button", "alert_box", "确 定");
 
-        this.removeEle("box_button", "alert_layer");
+        this.removeEle("box_button", "alert_layer", "remove-alert-box", 310);
 
-        // this.removeEle("alert_layer", "alert_layer");
+    };
+
+    Message.prototype.showRightBottom = function(option) {
+        var title = option.title || "";
+        var msg = option.msg || "默认值";
+        var showTime = option.showTime || 2;
+        var bgc = option.bgc || "rgba(0, 0, 0, .1)";
+
+        if (title !== "") {
+            this.createEle("div", "right-bottom-main right-bottom-main-show", "right_bottom_main", "body");
+            this.createEle("span", "right-bottom-text", "right_bottom_text", "right_bottom_main", title);
+            this.createEle("span", "right-bottom-hide", "right_bottom_hide", "right_bottom_main", "×");
+        } else {
+            //main-no-title 
+            this.createEle("div", "right-bottom-main main-no-title right-bottom-main-show ", "right_bottom_main", "body", bgc);
+            this.createEle("span", "right-bottom-hide no-title", "right_bottom_hide", "right_bottom_main", "×");
+        }
+        this.createEle("div", "right-bottom-centent", "right_bottom_centent", "right_bottom_main", msg);
+
+        this.removeEle("right_bottom_hide", "right_bottom_main", (title === "" ? "main-no-title " : "") + " right-bottom-main-hide", 300);
+        var a = this;
+        setTimeout(function() {
+            a.timeoutRemoveEle("right_bottom_main", (title === "" ? "main-no-title " : "") + " right-bottom-main-hide", 300);
+        }, showTime * 1000);
+
     };
 
     window.message_box = new Message();
